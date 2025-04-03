1e72f715652397fd86465be8d17183fdc75bf6e8
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-04-03 09:52:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,21 @@ mkdir -p "/opt/pipx/"
 # see: https://pypi.org/project/shrpid/
 PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin pipx install shrpid
 
-install -m 644 files/shrpid.service "/lib/systemd/system/"
+bash -c 'cat << EOF > /lib/systemd/system/shrpid.service
+[Unit]
+Description=SH-RPi Daemon
+After=syslog.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=root
+ExecStart=/usr/local/bin/shrpid -s /var/run/shrpid.sock
+
+[Install]
+WantedBy=multi-user.target
+EOF'
 
 #systemctl enable shrpid
