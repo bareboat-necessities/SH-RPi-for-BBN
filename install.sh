3f1bb185ca7555c8bd593c1859dd6606d9b58f82
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-04-03 09:49:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ apt-get -y install pipx
 
 mkdir -p "/opt/pipx/"
 
+# see: https://pypi.org/project/shrpid/
 PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin pipx install shrpid
 
 install -m 644 files/shrpid.service "/lib/systemd/system/"
